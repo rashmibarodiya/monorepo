ddb31cb514f9f0db650f1367bf71ec5d501742cb
Rashmi <165359201+rashmibarodiya@users.noreply.github.com>
2024-07-27 10:06:36 +0000

    signup + db functionality

--- turborepo/apps/myapp/src/app/admin/api/signup/route.ts
+++ turborepo/apps/myapp/src/app/admin/api/signup/route.ts
@@ -6,17 +6,33 @@ import {Admin} from '@repo/db/src/index'
 import{ connect} from '../../../../lib/dbConnnect'
 
 export async function POST(request: NextRequest) {
+    
     await connect();
     try {
+
+        //console.log(process.env.MONG!)
         const reqBody = await request.json();
         const { username, password } = reqBody;
         console.log(reqBody);
-
-        // Add your database logic here
-
-        return NextResponse.json({
-            msg: "Request processed successfully"
-        });
+        const admin = Admin.findOne(username)
+        if(admin){
+            return NextResponse.json({
+                msg : "Admin already exists"
+            }, {status : 500})
+        }else{
+            if(username && password){
+                const newAdmin = new Admin({username, password})
+                await newAdmin.save();
+                return NextResponse.json({
+                    msg : "Admin created successfully"
+                }, {status : 200})
+            }else{
+                return NextResponse.json({
+                    msg : "Username or password not provided"
+                },{status : 505})
+            }
+        }
+       
     } catch (error : any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
