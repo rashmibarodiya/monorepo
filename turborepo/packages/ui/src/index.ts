e081d255761f434c8ae0f9c1a2e6c0bdc0a63732
Rashmi <165359201+rashmibarodiya@users.noreply.github.com>
2024-07-28 08:46:51 +0000

    updated ui

---
+++ turborepo/packages/ui/src/index.ts
@@ -0,0 +1,12 @@
+
+
+// export * from "./button"
+// export * from "./signup"
+
+// export * from "./card"
+// export * from "./code"
+
+// "./button": "./src/button.tsx",
+    // "./card": "./src/card.tsx",
+    // "./code": "./src/code.tsx",
+    // "./signup": "./src/signup.tsx"
